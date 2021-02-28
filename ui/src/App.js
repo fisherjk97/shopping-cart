8493014c4b5485c9b5ac6c87ffb7aaeb11db3048
John Fisher <fisherjk97@gmail.com>
2021-02-28 16:29:31 -0700

    updating app file

--- ui/src/App.js
+++ ui/src/App.js
@@ -25,80 +25,12 @@ export const Beverage = () => (
 
 class App extends Component {
 
-  
-  
-  state = {
-    users: [],
-    products: [],
-    cart: []
-  };
-
-
-
-
-  
-  /*
-  async createUser() {
-    await axios.get(apiUrl + '/user-create');
-    this.loadUsers();
-  }
-
-  async loadUsers() {
-    const res = await axios.get(apiUrl + '/users');
-    this.setState({
-      users: res.data
-    });
-  }
-  */
-
-
-  async getProducts() {
-    await this.init(10);
-  }
-
-  componentDidMount() {
-    this.getProducts();
-  }
-
-  init(n){
-
-    var data = [];
-    for(var i = 0; i <  n; i++){
-      var product = (
-        { id: i+1 , 
-          name: "Product " + (i + 1),
-          description: "This is a toy",
-          price: 49.99
-        });
-  
-        data.push(product);
-    }
-    this.setState({
-      products: data
-    });
-  }
-  
-
 
   render() {
     return (
       <div className="App">
         <NavBar />
         <header className="App-header">
-
-
-          <Container>
-            <Row >
-              {
-              this.state.products.map(product => (
-                <Col xs="12" sm="12" md="6" lg="3" >
-                    <ProductCard  name={product.name} price={product.price} description={product.description}/>
-                </Col>
-
-              ))}
-              
-            </Row>
-        </Container>
         </header>
       </div>
     );
