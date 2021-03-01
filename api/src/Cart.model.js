1e6be4647d2ea573bbe90ae3db2cce1b187f1b56
John Fisher <fisherjk97@gmail.com>
2021-02-28 18:39:32 -0700

    working on cart

--- api/src/Cart.model.js
+++ api/src/Cart.model.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
+const Product = require("./src/Product.model");
 const cartSchema = new mongoose.Schema({
+    //items: [Product]
     productId: String,
     quantity: Number
 });
