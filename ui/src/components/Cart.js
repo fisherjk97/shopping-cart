d30a62c6f6048051cb40c6cc84f7c67bb5af85cf
John Fisher <fisherjk97@gmail.com>
2021-03-02 01:26:10 -0700

    added subtotals

--- ui/src/components/Cart.js
+++ ui/src/components/Cart.js
@@ -3,18 +3,51 @@ import axios from 'axios';
 import {Container, Row, Col} from "react-bootstrap"
 import  CartProductCard  from "./CartProductCard"
 import { connect } from 'react-redux'
+import NumberFormat from 'react-number-format';
+
 const apiUrl = `http://localhost:8080`;
 const productApiUrl = apiUrl + '/products';
 const getProductApiUrl = apiUrl + '/getProducts';
 const cartApiUrl = apiUrl + '/cart';
 
+export const SubTotal = (props) => (
+  <section>
+            <div className="banner-innerpage">
+            <div className="container">
+                <div className="row justify-content-center">
+                <div className="col-lg-12 align-self-center text-center">
+                    <h1 className="title">Summary</h1>
+                    <h2>${
+                      props.map(li => (li.price * li.quantity)).reduce((sum, val) => sum + val, 0)
+                      }
+                    </h2>
+                  </div>
+                </div>
+            </div>
+            </div>
+        </section>
+  
+)
+
 
 class Cart extends Component {
 
-    state = {
-        cart: this.props.cart,
-        products: []
-    };
+  
+  constructor(props)
+  {
+    super(props);
+    console.log(props)
+    this.state = { 
+      cart: this.props.cart,
+      sum:  0
+    }; 
+
+    this.getSubTotal = this.getSubTotal.bind(this)
+
+    
+  }
+
+  
 /*
     getCartProducts = async () => {
       const cart = await fetch(cartApiUrl).then(response => response.json());
@@ -33,41 +66,25 @@ class Cart extends Component {
     });
     }
 */
-
+/*
   componentDidMount() {
-    //this.getCartProducts();
+    this.getSubTotal();
     //this.getCartProducts();
   }
-
-
-  /*
-    async getCart() {
-      console.log("fetching cart");
-      //await this.init(10);
-      const res = await axios.get(apiUrl + '/products');
-          this.setState({
-              cart: res.data
-          });
-      console.log("done fetching cart");
-  }
-  
 */
-    getCartSize() {
-        //get actual cart size
-        return this.state.cart.length;
-    }
 
-    async updateQuantity() {
-      console.log("fetching products");
-      //await this.init(10);
-      const res = await axios.put(apiUrl + '/cart');
-          this.setState({
-              cart: res.data
-          });
-      console.log("done fetching products");
-    }
+  getSubTotal(){
+    
+    return this.state.cart.map(li => (li.price * li.quantity)).reduce((sum, val) => sum + val, 0)
+
+  }
+
+ 
+    
 
   render(){
+
+    const hasItems = (this.props.cart != null && this.props.cart.length > 0);
   return (
     <Container>
            <section>
@@ -92,14 +109,31 @@ class Cart extends Component {
               ))}
               
             </Row>
+            {hasItems? (
+            <div>
+              <h1>Subtotal</h1>
+              <h2>
+
+                        <NumberFormat value={
+                          this.props.cart.map(li => (li.price * li.quantity)).reduce((sum, val) => sum + val, 0)
+                          }
+                          displayType={'text'}
+                            thousandSeparator={true} 
+                            prefix={'$'} 
+                            />
+              </h2>
+            </div>
+            ) : ""}
         </Container>
+        
   );
 };
 }
 
 const mapStateToProps = state => {
   return { 
-    cart: state.cart 
+    cart: state.cart, 
+    sum: state.sum
   }
 }
 
--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -2,7 +2,9 @@ import { createStore } from 'redux'
 
 const initialState = {
     cart: [],
+    sum: 0
   }
+
   
   const reducer = (state = initialState, action) => {
   
@@ -11,24 +13,29 @@ const initialState = {
       
         return { 
             ...state,
-            cart: state.cart.concat(action.payload)
+            cart: state.cart.concat(action.payload),
+            //sum: state.cart.map(li => (li.price * li.quantity + 1)).reduce((sum, val) => sum + val, 0)
         }
     }
 
     if (action.type === 'UPDATE_QUANTITY') {
-        return {...state, cart: state.cart.map((product,index) => {
+        return {
+            ...state,
+            cart: state.cart.map((product,index) => {
             if (index === action.payload.index || (action.payload.id != null && product.id == action.payload.id)) {
               return {...product, quantity: product.quantity + action.payload.quantity}
             };
             return product;
-          })
+          }),
+          //sum: state.cart.map(li => (li.price * li.quantity)).reduce((sum, val) => sum + val, 0)
         }
     }
 
     if (action.type === 'REMOVE_FROM_CART') {
         return {
             ...state,
-            cart: [...state.cart.slice(0, action.payload.index), ...state.cart.slice(action.payload.index + 1)]
+            cart: [...state.cart.slice(0, action.payload.index), ...state.cart.slice(action.payload.index + 1)],
+            //sum: state.cart.map(li => (li.price * li.quantity)).reduce((sum, val) => sum + val, 0)
         }
         
     }
